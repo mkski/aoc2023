4a24cba0d53418b4917bd9108f97c13cf9437c33
Mike Kalaski <mikekalaski@gmail.com>
2023-12-09 15:11:59 -0500

    fore

--- day9/day9.py
+++ day9/day9.py
@@ -13,3 +13,9 @@ def prev_value(s):
 
 print(sum([next_value(s) for s in seqs]))
 print(sum([prev_value(s) for s in seqs]))
+
+i=open("input").read().split("\n")
+q=[list(map(int,s.split()))for s in i]
+n=lambda s:s[-1]+n([s[i]-s[i-1]for i in range(1,len(s))])if any(s)else 0
+p=lambda s:s[0]-p([s[i]-s[i-1]for i in range(1,len(s))])if any(s)else 0
+print(sum([n(s)for s in q]),sum([p(s)for s in q]))
