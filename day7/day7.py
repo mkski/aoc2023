f219f742c1c3d1ddba27dcceae2d6bee4ab545ad
Mike Kalaski <mikekalaski@gmail.com>
2023-12-09 20:49:19 -0500

    add day 7, just reverse input for day 9

--- day7/day7.py
+++ day7/day7.py
@@ -1,19 +1,40 @@
 import re
 from collections import Counter
+from functools import cmp_to_key, partial
 
-inp = open("test").read()
+inp = open("input").read()
+inp = re.findall(r"([AKQJT2-9]{5}) (\d+)", inp)
 ranks = "23456789TJQKA"
+ranks_joker = "J23456789TQKA"
+hand_ranks = [(1,1), (2,1), (2,2), (3,1), (3,2), (4,1), (5,), (5, 0)]
 
-def compare_hands(l, r):
-    # compare l to r
-    # return -1 if l < r
-    # return 0 if ==
-    # return 1 if l > r
-    pass
-
-for hand, wager in re.findall(r"([AKQJT2-9]{5}) (\d+)", inp):
-    print(hand)
+def get_hand_rank(hand, jokers=False):
     c = Counter(hand)
-    print(c.most_common(2))
-    print(wager)
-    print()
+    signature = tuple(i[1] for i in c.most_common(2))
+    if jokers:
+        if all(c == "J" for c in hand):
+            return hand_ranks.index((5, 0))
+        num_jokers = c["J"]
+        c.subtract({"J": num_jokers})
+        signature = tuple(i[1] for i in c.most_common(2))
+        signature = (signature[0] + num_jokers, signature[1])
+    return hand_ranks.index(signature)
+
+def tiebreak(l, r, jokers=False):
+    use_ranks = ranks_joker if jokers else ranks
+    for i in range(len(l)):
+        if l[i] == r[i]:
+            continue
+        return use_ranks.index(l[i]) - use_ranks.index(r[i])
+
+def compare_hands(l, r, jokers=False):
+    l, r = l[0], r[0]
+    left, right = get_hand_rank(l, jokers), get_hand_rank(r, jokers)
+    if left == right:
+        return tiebreak(l, r, jokers)
+    return left - right
+
+hands = sorted(inp, key=cmp_to_key(compare_hands))
+print(sum(rank * int(hand[1]) for rank, hand in enumerate(hands, 1)))
+hands = sorted(inp, key=cmp_to_key(partial(compare_hands, jokers=True)))
+print(sum(rank * int(hand[1]) for rank, hand in enumerate(hands, 1)))
--- day9/day9.py
+++ day9/day9.py
@@ -5,17 +5,11 @@ def next_value(s):
     if any(s):
         return s[-1] + next_value([s[i] - s[i-1] for i in range(1, len(s))])
     return 0
-    
-def prev_value(s):
-    if any(s):
-        return s[0] - prev_value([s[i] - s[i-1] for i in range(1, len(s))])
-    return 0
 
 print(sum([next_value(s) for s in seqs]))
-print(sum([prev_value(s) for s in seqs]))
+print(sum([next_value(s[::-1]) for s in seqs]))
 
 i=open("input").read().split("\n")
 q=[list(map(int,s.split()))for s in i]
 n=lambda s:s[-1]+n([s[i]-s[i-1]for i in range(1,len(s))])if any(s)else 0
-p=lambda s:s[0]-p([s[i]-s[i-1]for i in range(1,len(s))])if any(s)else 0
-print(sum([n(s)for s in q]),sum([p(s)for s in q]))
+print(sum([n(s)for s in q]),sum([n(s[::-1])for s in q]))
